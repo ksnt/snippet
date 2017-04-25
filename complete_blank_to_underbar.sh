467257be1704d7960d8ad83882b43d126c57a5d3
Takayuki Kaisen <ksn0215@gmail.com>
2017-04-25 17:58:06 +0900

    edit complete...sh

--- complete_blank_to_underbar.sh
+++ complete_blank_to_underbar.sh
@@ -1,1 +1,4 @@
-for A in $(find . | grep " " | sed -e s/" "/x3Exe/g) ; do mv "$(echo $A | sed -e s/x3Exe/' '/g)" "$(echo $A | sed -e s/x3Exe/'_'/g)"; done
+#/bin/sh
+for A in $(find . | grep " " | sed -e s/" "/x3Exe/g) ;\
+ do mv "$(echo $A | sed -e s/x3Exe/' '/g)" "$(echo $A | sed -e s/x3Exe/'_'/g)";\
+ done
